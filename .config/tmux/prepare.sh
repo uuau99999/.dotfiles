a0db360ba208f681318f9da0981dc5308731baf1
hoyup <hoyupye@tencent.com>
2024-06-18 20:05:17 +0800

    chore: 完善tmux sessionizer preview

--- .config/tmux/prepare.sh
+++ .config/tmux/prepare.sh
@@ -3,10 +3,4 @@
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-mkdir -p ~/.local/bin
-ln -s ~/.config/tmux/tmux-clear ~/.local/bin/tmux-clear
-ln -s ~/.config/tmux/tmux-sessionizer ~/.local/bin/tmux-sessionizer
-ln -s ~/.config/tmux/tmux-preview ~/.local/bin/tmux-preview
-
 touch ~/.tmux-repo
-echo ~/.config/tmux >>~/.tmux-repo
