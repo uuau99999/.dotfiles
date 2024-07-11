69adde6e1b54c93fc8f9e25de56333e4fea2ce94
hoyup <hoyupye@tencent.com>
2024-07-11 15:41:17 +0800

    chore: set wezterm startup auto maximized

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -40,5 +40,23 @@ config.colors = {
 	cursor_bg = "#f4dbd6",
 }
 
+-- This enable maximize window on startup
+wezterm.on("gui-startup", function(cmd)
+	-- Pick the active screen to maximize into, there are also other options, see the docs.
+	local active = wezterm.gui.screens().active
+
+	-- Set the window coords on spawn.
+	local window = wezterm.mux.spawn_window(cmd or {
+		x = active.x,
+		y = active.y,
+		width = active.width,
+		height = active.height,
+	})
+
+	-- You probably don't need both, but you can also set the positions after spawn.
+	window:gui_window():set_position(active.x, active.y)
+	window:gui_window():set_inner_size(active.width, active.height)
+end)
+
 -- and finally, return the configuration to wezterm
 return config
