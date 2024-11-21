b6f608f89f9537849bd9b825befa164f750e00b0
hoyup <hoyupye@tencent.com>
2024-11-21 19:20:21 +0800

    chore: add nix config - 5

--- nix/apps/nvim.nix
+++ nix/apps/nvim.nix
@@ -1,9 +1,14 @@
-{ pkgs,...}: 
+{ config, pkgs,...}: 
 { 
   home.packages = with pkgs; [lazygit];
 
   home.file = {
-    ".config/nvim".source = ../../.config/nvim;
+    # ".config/nvim".source = ../../.config/nvim;
+    ".config/nvim/init.lua".source = ../../.config/nvim/init.lua;
+    ".config/nvim/lua".source = ../../.config/nvim/lua;
+    ".config/nvim/stylua.toml".source = ../../.config/nvim/stylua.toml;
+    ".config/nvim/lazy-lock.json".source = config.lib.file.mkOutOfStoreSymlink "~/.dotfiles/.config/nvim/lazy-lock.json";
+    ".config/nvim/lazyvim.json".source = config.lib.file.mkOutOfStoreSymlink "~/.dotfiles/.config/nvim/lazyvim.json";
   };
 
   programs.neovim = {
