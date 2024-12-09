f53445fdd7b74522beeea1379644cdaf9d420660
hoyup <hoyupye@tencent.com>
2024-12-09 16:56:56 +0800

    fix: fix sketchybar aerospace plugin issues

--- .config/sketchybar/plugins/space_windows.sh
+++ .config/sketchybar/plugins/space_windows.sh
@@ -30,7 +30,7 @@ if [ "$SENDER" = "aerospace_workspace_change" ]; then
   # fi
   #space="$(echo "$INFO" | jq -r '.space')"
   #apps="$(echo "$INFO" | jq -r '.apps | keys[]')"
-  # apps=$(aerospace list-windows --workspace $AEROSPACE_FOCUSED_WORKSPACE | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+  # apps=$(aerospace list-windows --workspace $FOCUSED_WORKSPACE | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
   #
   # icon_strip=" "
   # if [ "${apps}" != "" ]; then
@@ -42,23 +42,26 @@ if [ "$SENDER" = "aerospace_workspace_change" ]; then
   #   icon_strip=" —"
   # fi
 
-  reload_workspace_icon "$AEROSPACE_PREV_WORKSPACE"
-  reload_workspace_icon "$AEROSPACE_FOCUSED_WORKSPACE"
+  # if [[ ! -z $AEROSPACE_PREV_WORKSPACE && "$AEROSPACE_PREV_WORKSPACE" != "$FOCUSED_WORKSPACE" ]]; then
+  #   reload_workspace_icon "$AEROSPACE_PREV_WORKSPACE"
+  #   # prev workspace space border color
+  #   sketchybar --set space.$AEROSPACE_PREV_WORKSPACE icon.highlight=false \
+  #     label.highlight=false \
+  #     background.border_color=$BACKGROUND_2
+  # fi
+  # AEROSPACE_PREV_WORKSPACE=$FOCUSED_WORKSPACE
+
+  reload_workspace_icon "$FOCUSED_WORKSPACE"
 
   #sketchybar --animate sin 10 --set space.$space label="$icon_strip"
 
   # current workspace space border color
-  sketchybar --set space.$AEROSPACE_FOCUSED_WORKSPACE icon.highlight=true \
+  sketchybar --set space.$FOCUSED_WORKSPACE icon.highlight=true \
     label.highlight=true \
     background.border_color=$GREY
 
-  # prev workspace space border color
-  sketchybar --set space.$AEROSPACE_PREV_WORKSPACE icon.highlight=false \
-    label.highlight=false \
-    background.border_color=$BACKGROUND_2
-
-  # if [ "$AEROSPACE_FOCUSED_WORKSPACE" -gt 3 ]; then
-  #   sketchybar --animate sin 10 --set space.$AEROSPACE_FOCUSED_WORKSPACE display=1
+  # if [ "$FOCUSED_WORKSPACE" -gt 3 ]; then
+  #   sketchybar --animate sin 10 --set space.$FOCUSED_WORKSPACE display=1
   # fi
   ## focused 된 모니터에 space 상태 보이게 설정
   for i in $AEROSAPCE_WORKSPACE_FOCUSED_MONITOR; do
@@ -69,6 +72,6 @@ if [ "$SENDER" = "aerospace_workspace_change" ]; then
     sketchybar --set space.$i display=0
   done
 
-  sketchybar --set space.$AEROSPACE_FOCUSED_WORKSPACE display=$AEROSPACE_FOCUSED_MONITOR
+  sketchybar --set space.$FOCUSED_WORKSPACE display=$AEROSPACE_FOCUSED_MONITOR
 
 fi
