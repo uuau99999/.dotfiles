d907eca1a24e8b4f2635a7c0c156336903f913a1
uuau99999 <uuau99999@gmail.com>
2025-04-24 14:48:12 +0800

    chore: disable avante - 2

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,7 +1,7 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
-  enable = false,
+  enabled = false,
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   tag = "v0.0.19",
