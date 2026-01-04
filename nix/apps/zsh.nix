fdbe208b8a8a5b217019cdd6f850c1091f1b3570
uuau99999 <uuau99999@gmail.com>
2026-01-04 19:01:11 +0800

    chore: update nix zsh path

--- nix/apps/zsh.nix
+++ nix/apps/zsh.nix
@@ -17,7 +17,7 @@
       export EDITOR=vim
       
       # Add any additional configurations here
-      export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
+      export PATH=$HOME/.local/bin:/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
       if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
         . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
       fi
