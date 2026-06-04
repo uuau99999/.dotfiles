6c934929ee9554d2c8a764ebbe2cba30e34a99a0
uuau99999 <uuau99999@gmail.com>
2026-06-04 16:53:09 +0800

    chore: disable leap capital S keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -169,7 +169,7 @@ nmap("<leader>o", require("mini.files").open, "Open mini-files")
 
 --leap
 vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)", { desc = "Leap" })
-vim.keymap.set("n", "S", "<Plug>(leap-from-window)", { desc = "Leap from window" })
+-- vim.keymap.set("n", "S", "<Plug>(leap-from-window)", { desc = "Leap from window" })
 
 --floaterm
 -- keymap.set("n", "<leader>p", "<cmd>FloatermToggle<cr>")
